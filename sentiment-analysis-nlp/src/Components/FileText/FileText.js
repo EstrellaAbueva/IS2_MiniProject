7ae87c484ca1593e71d46279944233f342101144
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-05-17 20:40:47 +0800

    Implement Partial UI

--- sentiment-analysis-nlp/src/Components/FileText/FileText.js
+++ sentiment-analysis-nlp/src/Components/FileText/FileText.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './index.css'
 
 export const FileText = () => {
   return (
-    <div>FileText</div>
+    <>
+      <div className='container'>
+        <div className='left-container'>
+          fesrfssfsf
+        </div>
+        <div className='right-container'>
+          sfsfsdf
+        </div>
+      </div>
+      <div className='container'>
+        <button className='import'>Import</button>
+        <button className='analyze'>Analyze</button>
+      </div>
+    </>
   )
 }
 
