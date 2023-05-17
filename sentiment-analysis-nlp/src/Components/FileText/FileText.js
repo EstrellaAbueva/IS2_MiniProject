d93db635bebb165237004005a0baf0b76987fb37
Kent Stephen Sumalinog <kentstephen.sumalinog@cit.edu>
2023-05-17 11:06:22 +0800

    Add menu functionality

---
+++ sentiment-analysis-nlp/src/Components/FileText/FileText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const FileText = () => {
+  return (
+    <div>FileText</div>
+  )
+}
+
+export default FileText;
--- sentiment-analysis-nlp/src/Components/Menu/Menu.js
+++ sentiment-analysis-nlp/src/Components/Menu/Menu.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react';
+import SingleText from '../SingleText/SingleText.js';
+import FileText from '../FileText/FileText.js';
+import Button from '../../Utils/Buttons/Button.js'
+
+import './index.css';
 
 export const Menu = () => {
+  const [button, setButton] = useState("");
+
+  function isClicked(text) {
+    if (text === "Single Text")
+      setButton("single-text");
+    else if (text === "File Text") {
+      setButton("file-text");
+    }
+  }
   return (
-    <div>Menu</div>
+    <div className="parent-bg">
+      <div className="button-box">
+        <Button text="Single Text" onClick={isClicked} />
+        <Button text="File Text" onClick={isClicked}/>
+      </div>
+      <div className="display">
+        {button === "single-text" ?
+          <SingleText /> :
+          button === "file-text" ?
+            <FileText /> :
+            null}
+      </div>
+    </div>
   )
 }
+
+export default Menu;
