8db4df41c4040b41a45d46f7131955a5c1c4490b
Kent Stephen Sumalinog <kentstephen.sumalinog@cit.edu>
2023-05-20 10:15:02 +0800

    Add Changes

--- sentiment-analysis-nlp/src/Components/SingleText/SingleText.js
+++ sentiment-analysis-nlp/src/Components/SingleText/SingleText.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 import './SingleText.css'
 import TextInput from '../TextInput/TextInput'
 
 export const SingleText = () => {
+  const [text, setText] = useState("");
+  const [clicked, setClicked] = useState(false);
+
   return (
       <div className='main-container'>
       <div className='left-container'>
-        <TextInput label={"Text to Analyze"}/>
+        <TextInput label={"Text to Analyze"} value={text} onChange={setText}/>
         <br />
-        <button className='analyze-button'>Analyze</button>
+        <button className='analyze-button' onClick={setClicked(true)}>Analyze</button>
       </div>
       <div className='right-container'>
         <div className='sentiment-image'>Emoji Pic</div>
