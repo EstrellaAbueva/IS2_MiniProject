ebca979d353390a1c8f9c442d4ce05aa4fdcb5eb
Estrella Abueva <estrella.abueva@cit.edu>
2023-05-20 12:51:16 +0800

    Added styling in the Menu

--- sentiment-analysis-nlp/src/Components/Menu/Menu.js
+++ sentiment-analysis-nlp/src/Components/Menu/Menu.js
@@ -4,23 +4,27 @@ import './Menu.css';
 export const Menu = () => {
   return (
     <div className="container">
-      <div className="content">
-      <h1 className="title">Sentiment Analysis App</h1>
-      <p className="text-block">
-        The Sentiment Analysis App Program is a powerful tool that analyzes the sentiment expressed in text data 
-        and provides valuable insights into the emotional tone and polarity of the input. 
-        By employing advanced natural language processing (NLP) techniques, the program can accurately 
-        determine whether a given text conveys the following emotions:
-      </p>
-      <ul className='list'>
-        <li>Anger</li>
-        <li>Fear</li>
-        <li>Joy</li>
-        <li>Love</li>
-        <li>Sadness</li>
-        <li>Surprise</li>
-        <li>Emotion Undefined</li>
-      </ul>
+        <div className="content">
+        <h1 className="one">
+            Sentiment Analysis App
+            <span className='one'>By: TEAM G</span>
+        </h1>
+        <p className="text-block">
+          The Sentiment Analysis App Program is a powerful tool that analyzes the sentiment expressed in text data 
+          and provides valuable insights into the emotional tone and polarity of the input. 
+          By employing advanced natural language processing (NLP) techniques, the program can accurately 
+          determine whether a given text conveys the following emotions:
+        </p>
+
+        <ol type='1' className='list'>
+          <li>Emotion Undefine</li>
+          <li>Joy</li>
+          <li>Love</li>
+          <li>Sadness</li>
+          <li>Anger</li>
+          <li>Fear</li>
+          <li>Surprise</li>
+        </ol>
       </div>
     </div>
   )
